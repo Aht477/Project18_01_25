65b7a7db9dc75737be86b6bab2da4ecbe5a7dfcc
aleks <aht477@gmail.com>
2025-01-18 19:14:40 +0100

    TestProject18_01_25

---
+++ src/testproectnewgrupp1/ClassTest1.java
@@ -0,0 +1,9 @@
+package testproectnewgrupp1;
+
+public class ClassTest1 {
+    public static void main(String[] args) {
+        System.out.println("Привет группа 1");
+
+
+    }
+}
---
+++ src/testproectnewgrupp1/MainApp.java
@@ -0,0 +1,8 @@
+package testproectnewgrupp1;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello GRUP!");
+    }
+}
